7374beb2a808286763ee5a058fbecc122b800654
Vinicius <21365373@anhembimorumbi.edu.br>
2021-10-07 18:01:24 -0300

    Adicionei musicas de place holder em todas as cenas(qualquer coisa é só tirar de alguma), Alterei o Script ChangeScene pra ele demorar 0.3f pra trocar de cena, assim deu pra tocar o som do botão.

--- Assets/Scripts/ChangerScene.cs
+++ Assets/Scripts/ChangerScene.cs
@@ -5,13 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class ChangerScene : MonoBehaviour
 {
+
+    public string nomeCena;
+
     private Animator começa;
 
     public void ChangeS(string gameplay)
     {
-        SceneManager.LoadScene(gameplay);
+        nomeCena = gameplay;
+        StartCoroutine("Abrir");
 
     }
+
+    private IEnumerator Abrir()
+    {
+        yield return new WaitForSeconds(0.3f);
+
+        SceneManager.LoadScene(nomeCena);
+    }
+       
 
     public void Sair()
     {
