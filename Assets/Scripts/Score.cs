b223b1f8498c4d7793a7f624ef1b864543ad8a6e
Alecsia Fernandes <alecsiafernandes@gmail.com>
2021-10-08 00:49:59 -0300

    Apenas salvando alteração no script.

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -6,8 +6,6 @@ using TMPro;
 public class Score : MonoBehaviour
 {
     private TextMeshProUGUI scoreText;
-    //public float valor;
-
     [SerializeField] private GameObject pause;
     [SerializeField] private Move move;
 
